9a60394d481240f95e37c3166af2c62466cd52bd
Michael Clarkson <clarksmr@gmail.com>
2018-08-15 17:44:20 -0400

    Install Batteries

--- user-setup.sh
+++ user-setup.sh
@@ -3,6 +3,6 @@ opam init -a -y --comp 4.06.1
 eval `opam config env`
 
 # Install OPAM packages
-opam install -y utop ounit qtest yojson lwt menhir ansiterminal lambda-term merlin ocp-indent user-setup coq coqide graphics
+opam install -y utop ounit qtest yojson lwt menhir ansiterminal lambda-term merlin ocp-indent user-setup coq coqide graphics batteries
 opam user-setup install
 
